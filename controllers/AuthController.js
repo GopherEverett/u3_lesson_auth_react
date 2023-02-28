7ded0a6fb97435fb0be16e634342f9ebf8d4b576
Michael Lackey <michael.g.lackey@gmail.com>
2023-02-28 16:41:25 -0500

    update starter

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,49 +1,72 @@
 const { User } = require('../models')
 const middleware = require('../middleware')
 
+const Register = async (req, res) => {
+  try {
+    // Extracts the necessary fields from the request body
+    const { email, password, name } = req.body
+    // Hashes the provided password
+    let passwordDigest = await middleware.hashPassword(password)
+    // Creates a new user
+    const user = await User.create({ email, passwordDigest, name })
+    // Sends the user as a response
+    res.send(user)
+  } catch (error) {
+    throw error
+  }
+}
+
 const Login = async (req, res) => {
   try {
+    // Finds a user by a particular field (in this case, email)
     const user = await User.findOne({
       where: { email: req.body.email },
       raw: true
     })
-    let isMatched = middleware.comparePassword(
+    // Checks if the password matches the stored digest
+    let matched = await middleware.comparePassword(
       user.passwordDigest,
       req.body.password
     )
-    if (isMatched) {
+    // If they match, constructs a payload object of values we want on the front end
+    if (matched) {
       let payload = {
         id: user.id,
         email: user.email
       }
+      // Creates our JWT and packages it with our payload to send as a response
       let token = middleware.createToken(payload)
       return res.send({ user: payload, token })
     }
-  } catch (error) {
     res.status(401).send({ status: 'Error', msg: 'Unauthorized' })
+  } catch (error) {
+    console.log(error)
+    res.status(401).send({ status: 'Error', msg: 'An error has occurred!' })
   }
-}
-
-const Register = async (req, res) => {
-  const { email, password, name } = req.body
-  let passwordDigest = await middleware.hashPassword(password)
-  const user = await User.create({ email, passwordDigest, name })
-  res.send(user)
 }
 
 const UpdatePassword = async (req, res) => {
-  const { oldPassword, newPassword } = req.body
-  const user = await User.findByPk(req.params.user_id)
-  let isMatched = middleware.comparePassword(
-    user.dataValues.passwordDigest,
-    oldPassword
-  )
-  if (isMatched) {
-    let passwordDigest = await middleware.hashPassword(newPassword)
-    await user.update({ passwordDigest })
-    return res.send({ status: 'Ok', payload: user })
+  try {
+    // Extracts the necessary fields from the request body
+    const { oldPassword, newPassword } = req.body
+    // Finds a user by a particular field (in this case, the user's id from the URL param)
+    const user = await User.findByPk(req.params.user_id)
+    // Checks if the password matches the stored digest
+    let matched = middleware.comparePassword(
+      user.dataValues.passwordDigest,
+      oldPassword
+    )
+    // If they match, hashes the new password, updates the db with the new digest, then sends the user as a response
+    if (matched) {
+      let passwordDigest = await middleware.hashPassword(newPassword)
+      await user.update({ passwordDigest })
+      return res.send({ status: 'Ok', payload: user })
+    }
+    res.status(401).send({ status: 'Error', msg: 'Unauthorized' })
+  } catch (error) {
+    console.log(error)
+    res.status(401).send({ status: 'Error', msg: 'An error has occurred!' })
   }
-  res.status(401).send({ status: 'Error', msg: 'Unauthorized' })
 }
 
 const CheckSession = async (req, res) => {
@@ -52,8 +75,8 @@ const CheckSession = async (req, res) => {
 }
 
 module.exports = {
-  Login,
   Register,
+  Login,
   UpdatePassword,
   CheckSession
 }
--- middleware/index.js
+++ middleware/index.js
@@ -1,13 +1,14 @@
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 require('dotenv').config()
+
 const SALT_ROUNDS = parseInt(process.env.SALT_ROUNDS)
 const APP_SECRET = process.env.APP_SECRET
 
 const hashPassword = async (password) => {
   // Accepts a password from the request body
   let hashedPassword = await bcrypt.hash(password, SALT_ROUNDS)
-  //   Creates a hashed password and encrypts it 12 times
+  // Creates a hashed password and encrypts it 12 times
   return hashedPassword
 }
 
@@ -15,30 +16,16 @@ const comparePassword = async (storedPassword, password) => {
   // Accepts the password provided in the login request and the currently stored password
   // Compares the two passwords for a match
   let passwordMatch = await bcrypt.compare(password, storedPassword)
-  //   returns true if the passwords match
-  // returns false if the passwords are not a match
+  // Returns true if the passwords match
+  // Returns false if the passwords are not a match
   return passwordMatch
 }
 
 const createToken = (payload) => {
   // Accepts a payload with which to create the token
   let token = jwt.sign(payload, APP_SECRET)
-  //   Generates the token and encrypts it, returns the token when the process finishes
+  // Generates the token and encrypts it, returns the token when the process finishes
   return token
-}
-
-const verifyToken = (req, res, next) => {
-  const { token } = res.locals
-  //   Gets the token stored in the request lifecycle state
-  let payload = jwt.verify(token, APP_SECRET)
-  //   Verifys the token is legit
-  if (payload) {
-    res.locals.payload = payload // Passes the decoded payload to the next function
-
-    //   Calls the next function if the token is valid
-    return next()
-  }
-  res.status(401).send({ status: 'Error', msg: 'Unauthorized' })
 }
 
 const stripToken = (req, res, next) => {
@@ -51,15 +38,35 @@ const stripToken = (req, res, next) => {
       //   If the token exists we add it to the request lifecycle state
       return next()
     }
-  } catch (error) {
     res.status(401).send({ status: 'Error', msg: 'Unauthorized' })
+  } catch (error) {
+    console.log(error)
+    res.status(401).send({ status: 'Error', msg: 'Strip Token Error!' })
+  }
+}
+
+const verifyToken = (req, res, next) => {
+  const { token } = res.locals
+  //   Gets the token stored in the request lifecycle state
+  try {
+    let payload = jwt.verify(token, APP_SECRET)
+    //   Verifies the token is legit
+    if (payload) {
+      res.locals.payload = payload // Passes the decoded payload to the next function
+      //   Calls the next function if the token is valid
+      return next()
+    }
+    res.status(401).send({ status: 'Error', msg: 'Unauthorized' })
+  } catch (error) {
+    console.log(error)
+    res.status(401).send({ status: 'Error', msg: 'Verify Token Error!' })
   }
 }
 
 module.exports = {
-  stripToken,
-  verifyToken,
-  createToken,
+  hashPassword,
   comparePassword,
-  hashPassword
+  createToken,
+  stripToken,
+  verifyToken
 }
